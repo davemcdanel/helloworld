cb3404d0b5f528d470ce2b1361cd95224a64e57e
Dave <dave@kc0bpo.lan>
2025-02-28 21:04:42 -0600

    Bump version.

--- Header_Files/version.h
+++ Header_Files/version.h
@@ -1,8 +1,8 @@
 /** @file version.h
- * @brief Version control file.
- * @version 0.0.24
+ * @brief Auto-generated version file.  Do not modify this file directly.
+ * @version 
  */
 #ifndef VERSION_H
 #define VERSION_H
-#define VERSION_STRING "0.1.6"
+#define VERSION_STRING "0.1.7"
 #endif
